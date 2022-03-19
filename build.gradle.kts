550e9d4ab9b188ce9bca2ab9d4fee5fdd3eaced2
Jonathan <jonathanlee06@outlook.com>
2022-03-19 10:48:37 +0800

    chore: Migrate project build.gradle to KTS 1. Added buildSrc to better manage dependencies 2. Bump compile & target SDK version to API 31

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Libraries.googleServices)
+        classpath(Libraries.firebase)
+        classpath(Libraries.gradle)
+        classpath(Libraries.kotlinGradlePlugin)
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
